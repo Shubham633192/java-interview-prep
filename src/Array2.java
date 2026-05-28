821e3da55694b16d5faa2a4e82f6e60b395acffd
Shubham633192 <shubhampappumishra2000@gmail.com>
2026-05-28 12:03:17 +0530

    Array builder

---
+++ src/Array2.java
@@ -0,0 +1,52 @@
+public class Array2 {
+    public static void main(String[] args) {
+
+        // Array declare
+        // int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        // System.out.println(arr[0]);
+        // System.out.println(arr[2]);
+
+        // System.out.println(arr.length);
+
+        // for (int i = 0; i < arr.length; i++) {
+        // System.out.println(i);
+        // }
+
+        // Lowest element
+
+        // int[] age = { 21, 22, 24, 26, 24 };
+        // int lowest = age[0];
+
+        // for (int i = 0; i < age.length; i++) {
+        // if (age[i] < lowest) {
+        // lowest = age[i];
+        // }
+        // }
+
+        // System.out.println(lowest);
+
+        // sum of element in array
+
+        // int[] num = { 1, 2, 3, 4, 5, 6, 7 };
+        // int sum = 0;
+        // for (int i = 0; i < num.length; i++) {
+        // sum += num[i];
+
+        // }
+
+        // System.out.println(sum);
+
+        // int[] num = { 1, 2, 3, 4, 5, 7, 8, 9 };
+        // int maximum = num[0];
+
+        // for (int i = 0; i < num.length; i++) {
+        // if (num[i] > maximum) {
+        // maximum = num[i];
+
+        // }
+
+        // }
+        // System.out.println(maximum);
+    }
+
+}
